e037c111e2671dda91acdf9e958bf1f51bcf7e3d
k-iaras-h <158334075+k-iaras-h@users.noreply.github.com>
2024-03-13 11:37:00 -0700

    Dashboard
    fixing resizing bugs and Adding slide show to Home page

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,45 +1,66 @@
-import React from "react"
-import Heading from "../../common/Heading"
-import "./hero.css"
+import React, { useState, useEffect } from "react";
+import Heading from "../../common/Heading";
+import "./hero.css";
+
+const images = [
+  "/images/p-9.jpg",  
+  "/images/p-2.jpg",
+  "/images/p-3.jpg",
+  "/images/p-4.jpg",
+  "/images/p-5.jpg",
+  "/images/p-6.jpg",
+  "/images/p-8.jpg",
+  "/images/p-7.jpg",
+  "/images/p-10.jpg",
+  "/images/p-1.jpg",
+
+];
 
 const Hero = () => {
+  const [currentBg, setCurrentBg] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentBg(currentBg => (currentBg + 1) % images.length);
+    }, 5000); // 
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
-      <section className='hero'>
+      <section className='hero' style={{ backgroundImage: `url(${images[currentBg]})` }}>
         <div className='container'>
-          <Heading title='Explore,Discover and wander with confidence ' subtitle='Traveling is not just about reaching a destination its about the experiences , memories and stories you will have along the way ' />
-
+          <Heading 
+            title='Explore, Discover and wander with confidence' 
+            subtitle='Traveling is not just about reaching a destination its about the experiences, memories and stories you will have along the way'
+            style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}
+          />
           <form className='flex'>
             <div className='box'>
               <span>From </span>
-              <input type='text' placeholder='.....' />
+              <input type='text' placeholder='Enter starting location...' />
             </div>
             <div className='box'>
               <span>To</span>
-              <input type='text' placeholder='.....' />
+              <input type='text' placeholder='Enter destination...' />
             </div>
             <div className='box'>
               <span>Arrival</span>
               <input type='date' placeholder='Arriving date' />
             </div>
             <div className='box'>
-              <span>Diparture</span>
+              <span>Departure</span>
               <input type='date' placeholder='Leaving date' />
             </div>
             <button className='btn1'>
-              <i className='fa fa-search'></i>
+              <i className='fa fa-search'></i> Search
             </button>
           </form>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-<Heading
-  title='Explore, Discover and wander with confidence'
-  subtitle='Traveling is not just about reaching a destination its about the experiences, memories and stories you will have along the way'
-  style={{ textShadow: '200 20 100px rgba(190, 19, 19, 0.879)' }}
-/>
-
-export default Hero
+export default Hero;
