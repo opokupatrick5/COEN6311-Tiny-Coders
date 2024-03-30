5d4825072d383b845504e264974d6f46a95aa4aa
Naoreen Chowdhury <naoreen.mmn@gmail.com>
2024-03-30 12:10:18 -0400

    Update urls.py

--- app/booking/urls.py
+++ app/booking/urls.py
@@ -1,8 +1,33 @@
 from django.urls import path
 from .views import UserRegistrationAPIView
 from .views import UserRegistrationAPIView, UserAuthenticationAPIView
+from .views import FlightListCreate, HotelListCreate, EditPackageAPIView, AddPackageAPIView, HotelRetrieveUpdateDestroy, ActivityListCreate, ActivityRetrieveUpdateDestroy, FlightRetrieveUpdateDestroy, PackageListCreate, PackageRetrieveUpdateDestroy, BookingListCreate, BookingRetrieveUpdateDestroy
+from .apiViews.flightAPIView import FlightAPIView
 
 urlpatterns = [
+        # auth URLs
     path('register/', UserRegistrationAPIView.as_view(), name='user-registration'),
     path('login/', UserAuthenticationAPIView.as_view(), name='user-login'),
+
+        # Flight URLs
+    path('flights/', FlightListCreate.as_view(), name='flight-list-create'),
+    path('flights/<int:pk>/', FlightRetrieveUpdateDestroy.as_view(), name='flight-retrieve-update-destroy'),
+
+    # Hotel URLs
+    path('hotels/', HotelListCreate.as_view(), name='hotel-list-create'),
+    path('hotels/<int:pk>/', HotelRetrieveUpdateDestroy.as_view(), name='hotel-retrieve-update-destroy'),
+
+    # Activity URLs
+    path('activities/', ActivityListCreate.as_view(), name='activity-list-create'),
+    path('activities/<int:pk>/', ActivityRetrieveUpdateDestroy.as_view(), name='activity-retrieve-update-destroy'),
+
+    # Package URLs
+    path('packages/add/', AddPackageAPIView.as_view(), name='add-package'),
+    path('packages/update/<int:pk>/', EditPackageAPIView.as_view(), name='edit-package'),
+    path('packages/', PackageListCreate.as_view(), name='package-list-create'),
+    path('packages/<int:pk>/', PackageRetrieveUpdateDestroy.as_view(), name='package-retrieve-update-destroy'),
+
+    # Booking URLs
+    path('bookings/', BookingListCreate.as_view(), name='booking-list-create'),
+    path('bookings/<int:pk>/', BookingRetrieveUpdateDestroy.as_view(), name='booking-retrieve-update-destroy')
 ]
